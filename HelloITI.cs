c01d92c8c8346e6a0f1a72900bc5f774d758a6fe
MiddleEnd-dev <mahmoud.talaat605@gmail.com>
2025-08-24 15:08:47 +0300

    commiting all files in repo

---
+++ HelloITI.cs
@@ -0,0 +1,10 @@
+﻿class HelloITI
+{     public static void Main()
+    {
+    printHelloITI();
+    }
+    public static void printHelloITI()
+    {
+        System.Console.WriteLine("Hello ITI");
+    }
+}
---
+++ HelloWorld.cs
@@ -0,0 +1,11 @@
+﻿class HelloWorld
+{
+    public static void Main()
+    {
+    printHelloWorld();
+    }
+    public static void printHelloWorld()
+    {
+        System.Console.WriteLine("Hello World");
+    }
+}
