8736d157961d0a981c0c3dd29e651c52ac91e7d3
MiddleEnd-dev <mahmoud.talaat605@gmail.com>
2025-08-25 12:29:28 +0300

    added HelloITI.cs and HelloSwift.cs to main branch

---
+++ HelloITI.cs
@@ -0,0 +1,14 @@
+﻿class HelloITI
+{     public static void Main()
+    {
+    printHelloITI();
+    }
+    public static void printHelloITI()
+    {
+        System.Console.WriteLine("Hello ITI");
+    }
+	public static void printHelloGit()
+    {
+    Console.WriteLine("HelloGit");
+    }
+}
